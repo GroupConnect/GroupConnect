725db41d4ebd7c4014457b195ef04743f527f701
CyberTorte <kbc16a07@stu.kawahara.ac.jp>
2018-10-03 15:30:12 +0900

    [add] トップページのテスト追加

--- GroupConnect/tests.py
+++ GroupConnect/tests.py
@@ -1,7 +1,14 @@
+from django.test import client
+from django.urls import reverse
+
 import unittest
 
 # Create your tests here.
 
-class TopPageTest(TestCase):
-    def test_show_page(self):
-
+class TopPageTest(unittest.TestCase):
+    def test_show_top_page(self):
+        response = self.client.get('GroupConnect:index')
+        self.assertEqual(
+            response.status_code,
+            200
+        )
