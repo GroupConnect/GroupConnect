37912721e97af036165634c6aae7ddee7f0a399e
Yuma Hongu <kbc16a18@stu.kawahara.ac.jp>
2018-11-02 10:12:22 +0900

    test

--- GroupConnect/views.py
+++ GroupConnect/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import (
     LoginView, LogoutView
@@ -7,6 +8,8 @@ from django.views import generic
 from .forms import (
     LoginForm, UserCreateForm
 )
+from .models import Account
+
 # Create your views here.
 
 class IndexView(generic.TemplateView):
@@ -27,3 +30,8 @@ class UserCreate(generic.CreateView):
     form_class = UserCreateForm
     template_name = 'GroupConnect/user_create.html'
 
+    def form_valid(self, form):
+        account = form.save(commit=False)
+        account.save()
+
+        return redirect('GroupConnect:top')
