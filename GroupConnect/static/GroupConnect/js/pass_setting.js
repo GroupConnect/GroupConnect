b44cf9b74fbf1ad91b67fefeb76186dd0782ff03
kbc16a03 <kbc16a03@stu.kawahara.ac.jp>
2019-01-11 14:02:24 +0900

    [update]掲示板作成ポップアップの作成

---
+++ GroupConnect/static/GroupConnect/js/pass_setting.js
@@ -0,0 +1,193 @@
+var countUpValue = 0;
+$(function() {
+    $('#passcheck').click(function countUp(){
+        countUpValue++;
+        if ( ( countUpValue % 2 ) != 0  ) {
+            $('#currentpass').attr('type','text');
+        } else {
+            $('#currentpass').attr('type','password');
+        }
+    });
+});
+
+var countUpValue2 = 0;
+$(function() {
+    $('#passcheck2').click(function countUp(){
+        countUpValue2++;
+        if ( ( countUpValue2 % 2 ) != 0  ) {
+            $('#pass').attr('type','text');
+        } else {
+            $('#pass').attr('type','password');
+        }
+    });
+});
+
+var countUpValue3 = 0;
+$(function() {
+    $('#passcheck3').click(function countUp(){
+        countUpValue3++;
+        if ( ( countUpValue3 % 2 ) != 0  ) {
+            $('#passconf').attr('type','text');
+        } else {
+            $('#passconf').attr('type','password');
+        }
+    });
+});
+
+$(function() {
+    $("form").submit(function(){
+    if ($("input[name='currentpass']").val() == '' && $("input[name='pass']").val() == '' && $("input[name='passconf']").val() == '') {
+        //現在のパスワード、新しいパスワード、新しいパスワード(確認用)が入力されていない場合
+        $('.currentpassblank').show();
+        $('.passblank').show();
+        $('.passconfblank').show();
+        $('.currentpassformatcheck').hide();
+        $('.passformatcheck').hide();
+        $('.passconfformatcheck').hide();
+        $('.passconfdifferent').hide();
+        return false;
+    }else if ($("input[name='currentpass']").val() == '' && $("input[name='pass']").val() == '') {
+        //現在のパスワード、新しいパスワードが入力されていない場合
+        $('.currentpassblank').show();
+        $('.passblank').show();
+        $('.passconfblank').hide();
+        $('.currentpassformatcheck').hide();
+        $('.passformatcheck').hide();
+        $('.passconfformatcheck').hide();
+        $('.passconfdifferent').hide();
+        return false;
+    }else if ($("input[name='currentpass']").val() == '' && $("input[name='passconf']").val() == '') {
+        //現在のパスワード、新しいパスワード(確認用)が入力されていない場合
+        $('.currentpassblank').show();
+        $('.passblank').hide();
+        $('.passconfblank').show();
+        $('.currentpassformatcheck').hide();
+        $('.passformatcheck').hide();
+        $('.passconfformatcheck').hide();
+        $('.passconfdifferent').hide();
+        return false;
+    }else if ($("input[name='pass']").val() == '' && $("input[name='passconf']").val() == '') {
+        //新しいパスワード、新しいパスワード(確認用)が入力されていない場合
+        $('.currentpassblank').hide();
+        $('.passblank').show();
+        $('.passconfblank').show();
+        $('.currentpassformatcheck').hide();
+        $('.passformatcheck').hide();
+        $('.passconfformatcheck').hide();
+        $('.passconfdifferent').hide();
+        return false;
+    } else if ($("input[name='currentpass']").val() == '') {
+        //現在のパスワードが入力されていない場合
+        $('.currentpassblank').show();
+        $('.passblank').hide();
+        $('.passconfblank').hide();
+        $('.currentpassformatcheck').hide();
+        $('.passformatcheck').hide();
+        $('.passconfformatcheck').hide();
+        $('.passconfdifferent').hide();
+        return false;
+    } else if ($("input[name='pass']").val() == '') {
+        //新しいパスワードが入力されていない場合
+        $('.currentpassblank').hide();
+        $('.passblank').show();
+        $('.passconfblank').hide();
+        $('.currentpassformatcheck').hide();
+        $('.passformatcheck').hide();
+        $('.passconfformatcheck').hide();
+        $('.passconfdifferent').hide();
+        return false;
+    } else if ($("input[name='passconf']").val() == '') {
+        //新しいパスワード(確認用)が入力されていない場合
+        $('.currentpassblank').hide();
+        $('.passblank').hide();
+        $('.passconfblank').show();
+        $('.currentpassformatcheck').hide();
+        $('.passformatcheck').hide();
+        $('.passconfformatcheck').hide();
+        $('.passconfdifferent').hide();
+        return false;
+    } else if (!$("input[name='currentpass']").val().match(/^[0-9a-zA-Z]{8,32}$/) && !$("input[name='pass']").val().match(/^[0-9a-zA-Z]{8,32}$/) && !$("input[name='passconf']").val().match(/^[0-9a-zA-Z]{8,32}$/)) {
+        //現在のパスワード、新しいパスワード、新しいパスワード(確認用)の文字数が違う場合
+        $('.currentpassblank').hide();
+        $('.passblank').hide();
+        $('.passconfblank').hide();
+        $('.currentpassformatcheck').show();
+        $('.passformatcheck').show();
+        $('.passconfformatcheck').show();
+        $('.passconfdifferent').hide();
+        return false;
+    } else if (!$("input[name='currentpass']").val().match(/^[0-9a-zA-Z]{8,32}$/) && !$("input[name='pass']").val().match(/^[0-9a-zA-Z]{8,32}$/)) {
+        //現在のパスワードと新しいパスワードの文字数が違う場合
+        $('.currentpassblank').show();
+        $('.passblank').show();
+        $('.passconfblank').hide();
+        $('.currentpassformatcheck').hide();
+        $('.passformatcheck').hide();
+        $('.passconfformatcheck').hide();
+        $('.passconfdifferent').hide();
+        return false;
+    } else if (!$("input[name='currentpass']").val().match(/^[0-9a-zA-Z]{8,32}$/) && !$("input[name='passconf']").val().match(/^[0-9a-zA-Z]{8,32}$/)) {
+        //現在のパスワードと新しいパスワード(確認用)の文字数が違う場合
+        $('.currentpassblank').hide();
+        $('.passblank').hide();
+        $('.passconfblank').hide();
+        $('.currentpassformatcheck').show();
+        $('.passformatcheck').hide();
+        $('.passconfformatcheck').show();
+        $('.passconfdifferent').hide();
+        return false;
+    } else if (!$("input[name='pass']").val().match(/^[0-9a-zA-Z]{8,32}$/) && !$("input[name='passconf']").val().match(/^[0-9a-zA-Z]{8,32}$/)) {
+        //パスワードとパスワード(確認用)の文字数が違う場合
+        $('.currentpassblank').hide();
+        $('.passblank').hide();
+        $('.passconfblank').hide();
+        $('.currentpassformatcheck').hide();
+        $('.passformatcheck').show();
+        $('.passconfformatcheck').show();
+        $('.passconfdifferent').hide();
+        return false;
+    } else if (!$("input[name='currentpass']").val().match(/^[0-9a-zA-Z]{8,32}$/)) {
+        //パスワードの文字数が違う場合
+        $('.currentpassblank').hide();
+        $('.passblank').hide();
+        $('.passconfblank').hide();
+        $('.currentpassformatcheck').show();
+        $('.passformatcheck').hide();
+        $('.passconfformatcheck').hide();
+        $('.passconfdifferent').hide();
+        return false;
+    } else if (!$("input[name='pass']").val().match(/^[0-9a-zA-Z]{8,32}$/)) {
+        //パスワードの文字数が違う場合
+        $('.currentpassblank').hide();
+        $('.passblank').hide();
+        $('.passconfblank').hide();
+        $('.currentpassformatcheck').hide();
+        $('.passformatcheck').show();
+        $('.passconfformatcheck').hide();
+        $('.passconfdifferent').hide();
+        return false;
+    } else if (!$("input[name='passconf']").val().match(/^[0-9a-zA-Z]{8,32}$/)) {
+        //パスワード(確認用)の文字数が違う場合
+        $('.currentpassblank').hide();
+        $('.passblank').hide();
+        $('.passconfblank').hide();
+        $('.currentpassformatcheck').hide();
+        $('.passformatcheck').hide();
+        $('.passconfformatcheck').show();
+        $('.passconfdifferent').hide();
+        return false;
+    } else if ($("input[name='pass']").val() !== $("input[name='passconf']").val()) {
+        //パスワードとパスワード(確認用)が異なる場合
+        $('.currentpassblank').hide();
+        $('.passblank').hide();
+        $('.passconfblank').hide();
+        $('.currentpassformatcheck').hide();
+        $('.passformatcheck').hide();
+        $('.passconfformatcheck').hide();
+        $('.passconfdifferent').show();
+        return false;
+    } else {
+      $("form").submit();
+    }
+});
+}); 
--- GroupConnect/static/GroupConnect/js/top.js
+++ GroupConnect/static/GroupConnect/js/top.js
@@ -21,7 +21,7 @@ $(function() {
             $('.mailformatcheck').hide();
             $('.passformatcheck').hide();
             return false; 
-        } else if (!$("input[name='mail']").val().match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/) && !$("input[name='pass']").val().match(/^[0-9a-zA-Z]{8}$/)){
+        } else if (!$("input[name='mail']").val().match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/) && !$("input[name='pass']").val().match(/^[0-9a-zA-Z]{8,32}$/)){
             //メールの形式とパスワードの文字数、形式が違う場合
             $('.mailblank').hide();
             $('.passblank').hide();
@@ -35,7 +35,7 @@ $(function() {
             $('.mailformatcheck').show();
             $('.passformatcheck').hide();
             return false;
-        } else if (!$("input[name='pass']").val().match(/^[0-9a-zA-Z]{8}$/)) {
+        } else if (!$("input[name='pass']").val().match(/^[0-9a-zA-Z]{8,32}$/)) {
             //パスワードの文字数、形式が違う場合
             $('.mailblank').hide();
             $('.passblank').hide();
