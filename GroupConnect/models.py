6213b413bd4735a51b6ac7d5a261ffec84ae8682
CyberTorte <endless.story.ms@gmail.com>
2019-02-07 13:57:55 +0900

    [update] diff_dateを掲示板クラスに追加、投稿クラスの同関数修正

--- GroupConnect/models.py
+++ GroupConnect/models.py
@@ -202,6 +202,24 @@ class Signboard(models.Model):
         """
         return self.title
 
+    def diff_date(self):
+        diff_words = ['日前', '時間前', '分前', '秒前']
+
+        now = timezone.now()
+        diff_date = now - self.updated_at()
+        diff_hours = diff_date.seconds / 60 / 60
+        diff_minutes = diff_date.seconds / 60
+
+        if (6 < diff_date.days):
+            return self.updated_at.date()
+        elif (0 < diff_date.days):
+            return str(diff_date.days) + diff_words[0]
+        elif (0 < diff_hours):
+            return str(diff_hours) + diff_words[1]
+        elif (0 < diff_minutes):
+            return str(diff_minutes) + diff_words[2]
+        elif (0 < diff_date.seconds):
+            return str(diff_date.seconds) + diff_words[3]
 
 class Post(models.Model):
     """
@@ -240,7 +258,7 @@ class Post(models.Model):
         diff_minutes = diff_date.seconds / 60
 
         if (6 < diff_date.days):
-            return self.updated_at.date()
+            return self.created_at.date()
         elif (0 < diff_date.days):
             return str(diff_date.days) + diff_words[0]
         elif (0 < diff_hours):
