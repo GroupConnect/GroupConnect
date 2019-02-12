18c57b587579c9a2b6941a0ffb9502472ca760a5
Yuma Hongu <kbc16a18@stu.kawahara.ac.jp>
2019-02-12 22:11:39 +0900

    グループ名の表示に文字数制限追加

--- GroupConnect/static/GroupConnect/js/mypage.js
+++ GroupConnect/static/GroupConnect/js/mypage.js
@@ -4,3 +4,20 @@ $(function(){
         visibleItem.text($(this).attr('value'));
     });
 });
+
+$(function(){
+    var $setText = $('ul li .name_limit');
+    var cutFigure = '5'; // 表示する文字数の設定
+    var afterText = ' …'; // 省略した末尾に追加するテキスト
+
+    $setText.each(function(){
+        var textLength = $(this).text().length;
+        var textTrim = $(this).text().substr(0,(cutFigure))
+
+        if(cutFigure < textLength) {
+            $(this).html(textTrim + afterText).css({visibility:'visible'});
+        } else if(cutFigure >= textLength) {
+            $(this).css({visibility:'visible'});
+        }
+    });
+});
